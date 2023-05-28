0cc09ddc435c12e827b6938dd9c971e67166732e
Hye  Mee Park <hyemee1021@gmail.com>
2023-05-28 13:13:55 +1000

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+// export function Footer() {
+//   return <p>This is a footer</p>;
+// }
+
+const Footer = () => {
+  return <h3>Footer</h3>;
+};
+
+export default Footer;
+// another way to import
